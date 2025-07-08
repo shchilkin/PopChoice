a1563d07ce8b45b9b61424fa33fd5372cdb78d1f
Aleksandr Shchilkin <shchilkin@gmail.com>
2025-07-08 18:15:56 +0300

    pop-choice-1: add stories for questions page

---
+++ src/app/QuestionsPage.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import QuestionsPage from "./page";
+
+export default {
+  title: "Pages/Questions Page",
+  component: QuestionsPage,
+};
+
+export const Default = () => <QuestionsPage />;
