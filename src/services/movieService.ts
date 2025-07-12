edd28e21968a8a6eee19d4967fa11c0255e1565a
Aleksandr Shchilkin <shchilkin@gmail.com>
2025-07-12 13:57:25 +0000

    pop-choice-22: add Movie Service Starter

---
+++ src/services/movieService.ts
@@ -0,0 +1,93 @@
+import z from 'zod/v4';
+import axios, { AxiosInstance } from 'axios';
+
+const POSTER_SIZES = ['w92', 'w154', 'w185', 'w342', 'w500', 'w780', 'original'] as const;
+
+const posterSize = z.enum(POSTER_SIZES).default('original');
+
+// TODO: Move to types after service is fully implemented
+export type TMDB_MovieEntry = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
+// TODO: Move to types after service is fully implemented
+type PosterSize = z.infer<typeof posterSize>;
+
+export default class MovieService {
+  private axiosClient: AxiosInstance;
+  private imageURLBase: string;
+  private apiURLBase: string;
+
+  constructor() {
+    this.axiosClient = axios.create();
+    this.imageURLBase = 'https://image.tmdb.org/t/p';
+    this.apiURLBase = 'https://api.themoviedb.org/3';
+  }
+
+  async getMovieByTitle(movieTitle: string): Promise<TMDB_MovieEntry | undefined> {
+    // TODO: Implement getMovieByTitle
+
+    const response = await this.axiosClient({
+      method: 'GET',
+      url: `${this.apiURLBase}/search/movie`,
+      responseType: 'json',
+      headers: {
+        Authorization: `Bearer ${process.env.TMDB_API_KEY}`,
+      },
+      params: {
+        query: movieTitle,
+        language: 'en-US',
+        include_adult: false,
+      },
+    })
+      .then((response) => {
+        const results = response.data.results;
+        const filteredResults = results.filter((movie: TMDB_MovieEntry) => {
+          return movie.title.toLowerCase() === movieTitle.toLowerCase();
+        });
+        if (filteredResults.length === 0) {
+          console.warn(`No movie found with title: ${movieTitle}`);
+          return [];
+        }
+        return filteredResults[0];
+      })
+      .catch((error) => {
+        console.error('Error fetching movie by title:', error);
+        throw new Error(`Failed to fetch movie by title: ${movieTitle}`);
+      });
+    return response;
+  }
+
+  getPosterURL(posterPath: string, size: PosterSize) {
+    const { success, data: parsedSize } = posterSize.safeParse(size);
+    if (!success) {
+      return new Error(`Invalid poster size: ${size}. Available sizes: ${POSTER_SIZES.join(', ')}`);
+    }
+    return `${this.imageURLBase}/${parsedSize}${posterPath}`;
+  }
+}
+
+// TODO: Remove after service is fully implemented and integrated to app
+const movieService = new MovieService();
+
+// TODO: Remove after service is fully implemented and integrated to app
+const result = await movieService.getMovieByTitle('The martian');
+
+// Example usage of getPosterURL
+// TODO: Remove after service is fully implemented and integrated to app
+if (result) {
+  console.log('Poster', movieService.getPosterURL(result.poster_path, 'w500'));
+}
