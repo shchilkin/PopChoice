68cf79d1f374fc73fcc61be3c1ec7b8371de1b4a
Aleksandr Shchilkin <shchilkin@gmail.com>
2025-07-21 17:37:20 +0000

    Add yml files parsing

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 const lintStagedConfig = {
   '*.{js,jsx,ts,tsx,mjs}': ['eslint --fix', 'prettier --write'],
-  '*.{json,css,md}': ['prettier --write'],
+  '*.{json,css,md,yml,yaml}': ['prettier --write'],
   'package.json': ['sort-package-json', 'prettier --write'],
 };
 
